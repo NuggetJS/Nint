e1b9638945aed6915caff07c33b9586d3747f533
NuggetJS <96446519+NuggetJS@users.noreply.github.com>
2021-12-21 19:15:51 +0100

    Add files via upload

--- script/functions.js
+++ script/functions.js
@@ -8,27 +8,27 @@ function speak(spk) {
     
 }
 
-function escribir (text,tiempo, etiqueta){
-    let arrayCaracteres = text.split('')
-    etiqueta.innerHTML = ''
-    let i = 0
-    let j = text.length
-    let escribir = setInterval(function(){
-      if (i === arrayCaracteres.length) {
-        etiqueta.innerHTML = text.substring(0,j)
+// function escribir (text,tiempo, etiqueta){
+//     let arrayCaracteres = text.split('')
+//     etiqueta.innerHTML = ''
+//     let i = 0
+//     let j = text.length
+//     let escribir = setInterval(function(){
+//       if (i === arrayCaracteres.length) {
+//         etiqueta.innerHTML = text.substring(0,j)
         
-        if (j === 0) {
-          etiqueta.innerHTML = ''
-          i = 0
-          j = text.length
-        }
-      } else {
-        etiqueta.innerHTML += arrayCaracteres[i]
-        i++
-      }
+//         if (j === 0) {
+//           etiqueta.innerHTML = ''
+//           i = 0
+//           j = text.length
+//         }
+//       } else {
+//         etiqueta.innerHTML += arrayCaracteres[i]
+//         i++
+//       }
        
-    }, tiempo)
-}
+//     }, tiempo)
+// }
 function hablar(respuesta) {  
   text.nintTxt.innerHTML = "";
     respuesta = new SpeechSynthesisUtterance(respuesta);
@@ -37,5 +37,5 @@ function hablar(respuesta) {
     speechSynthesis.speak(respuesta);
     // animacionHablar(Anim);
     console.log(respuesta.text); 
-    escribir(respuesta.text,50,text.nintTxt);
+    text.nintTxt.innerHTML = respuesta.text;
   }
